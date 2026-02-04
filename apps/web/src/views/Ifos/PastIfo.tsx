6ec173ddce4b9fe2342c38271d9d017489bdf741
memoyil <2213635+memoyil@users.noreply.github.com>
2026-02-04 03:27:15 +0100

    feat: Redirect old ifos to legacy site (#12619)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR focuses on refactoring the `PastIfo` component by removing
    unused imports, updating the styling of the `Header`, and enhancing the
    `PastIfoCard` to include a link. It also modifies the rendering logic to
    filter out testnet IFos.
    
    ### Detailed summary
    - Removed `Box` and `StyledCard` components.
    - Updated `Header` styling with background properties.
    - Changed `PastIfoCard` to use `NextLinkFromReactRouter` for linking to
    history.
    - Modified `Container` to include a flex layout and gap.
    - Filtered out testnet IFos in the mapping function.
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- apps/web/src/views/Ifos/PastIfo.tsx
+++ apps/web/src/views/Ifos/PastIfo.tsx
@@ -1,49 +1,52 @@
 import { Ifo } from '@pancakeswap/ifos'
-import { Box, Card, CardHeader, Container } from '@pancakeswap/uikit'
+import { CardHeader, Container } from '@pancakeswap/uikit'
 
 import { useInActiveIfoConfigs } from 'hooks/useIfoConfig'
 
 import HistoryIfos from 'views/Cakepad/HistoryIfos'
 import { styled } from 'styled-components'
+import { NextLinkFromReactRouter } from '@pancakeswap/widgets-internal'
+import { chainNames, isTestnetChainId } from '@pancakeswap/chains'
 import { getBannerUrl } from './helpers'
 
-const StyledCard = styled(Card)`
-  width: 100%;
-  margin: auto;
-  border-radius: 32px;
-`
-
 const Header = styled(CardHeader)<{ ifoId: string }>`
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
   height: 112px;
+  border-radius: 32px;
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
-  border-top-left-radius: 32px;
-  border-top-right-radius: 32px;
   background-color: ${({ theme }) => theme.colors.dropdown};
   background-image: ${({ ifoId }) => `url('${getBannerUrl(ifoId)}')`};
 `
 
-const PastIfoCard = ({ ifo }: { ifo: Ifo }) => (
-  <Box id={ifo.id} position="relative" mb="10px">
-    <StyledCard>
+const PastIfoCard = ({ ifo }: { ifo: Ifo }) => {
+  return (
+    <NextLinkFromReactRouter
+      to={`https://legacy-zkevm.pancakeswap.finance/ifo/history?chain=${chainNames[ifo.chainId]}`}
+      target="_blank"
+      rel="noreferrer noopener"
+    >
       <Header ifoId={ifo.id} />
-    </StyledCard>
-  </Box>
-)
+    </NextLinkFromReactRouter>
+  )
+}
 
 const PastIfo = ({ isV2 }: { isV2?: boolean }) => {
   const inactiveIfo = useInActiveIfoConfigs()
 
   return (
-    <Container id="past-ifos" py={['24px', '24px', '40px']} maxWidth="736px" m="auto" width="100%">
+    <Container
+      id="past-ifos"
+      py={['24px', '24px', '40px']}
+      maxWidth="736px"
+      style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}
+    >
       {isV2 && <HistoryIfos />}
-      {inactiveIfo?.map((ifo) => (
-        <PastIfoCard key={ifo.id} ifo={ifo} />
-      ))}
+      {inactiveIfo
+        ?.filter((ifo) => !isTestnetChainId(ifo.chainId))
+        .map((ifo) => (
+          <PastIfoCard key={ifo.id} ifo={ifo} />
+        ))}
     </Container>
   )
 }
